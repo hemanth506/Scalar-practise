57b727055546cce1c0eee281f9c617305bc776d0
G.Hemanth Raaj <hemanthraaj31@gmail.com>
2024-04-19 23:59:00 +0530

    Medium - Hashmap

---
+++ LeetCode/Medium/findAllLonelyNumbers_Q2150.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class findAllLonelyNumbers_Q2150 {
+  public List<Integer> findLonely(int[] nums) {
+    int N = nums.length;
+    HashMap<Integer, Integer> hs = new HashMap<>();
+
+    for (int i = 0; i < N; i++) {
+      int val = hs.getOrDefault(nums[i], 0);
+      hs.put(nums[i], val+1);
+    }
+
+    List<Integer> arr = new ArrayList<Integer>();
+    for (int i = 0; i < N; i++) {
+      int count = hs.get(nums[i]);
+      if (count == 1 && !hs.containsKey(nums[i] + 1) && !hs.containsKey(nums[i] - 1)) {
+        arr.add(nums[i]);
+      }
+    }
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    findAllLonelyNumbers_Q2150 fd = new findAllLonelyNumbers_Q2150();
+    int[] nums = { 1,3,5,3 };
+    fd.findLonely(nums);
+  }
+}
